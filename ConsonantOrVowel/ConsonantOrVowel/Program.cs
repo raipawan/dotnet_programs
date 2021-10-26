0867d47bde8a419b6030d8c2359f4e025a9972f2
pavan kumar <raipawan337@gmail.com>
2021-10-26 23:12:34 +0530

    added two new files

---
+++ ConsonantOrVowel/ConsonantOrVowel/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsonantOrVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter any character: ");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+
+                Console.WriteLine(ch + " is Vowel.");
+
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine(ch + " is Consonant.");
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+        }
+    }
+}
---
+++ EvenOdd/EvenOdd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Odd Number");
+            }
+        }
+    }
+}
