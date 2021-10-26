e547e8eeb088c2bb82835c846efa72211f08b48a
pavan kumar <raipawan337@gmail.com>
2021-10-26 13:43:43 +0530

    added file maxoftreenumbers

---
+++ maxOfThreeNumbers/maxOfThreeNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace maxOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter third number");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("Number one" + num1 + " is the largest\n");
+                }
+                else
+                {
+                    Console.Write("Number three" + num3 + " is the largest!\n");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("Number two" + num2 + " is the largest!\n");
+            else
+                Console.Write("Number three" + num3 + " is the largest!\n");
+        }
+    }
+}
