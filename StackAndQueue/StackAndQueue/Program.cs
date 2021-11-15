90d66d75e7ba04082365127ba30e28f1b2c5fc32
pavan kumar <raipawan337@gmail.com>
2021-11-15 15:09:49 +0530

    updated

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -7,27 +7,17 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
             /* StackUsingLinkedList my_stack = new StackUsingLinkedList();
-             my_stack.push(70);
-             my_stack.push(40);
-             my_stack.push(77);
-             my_stack.push(798);
-             my_stack.pop();
-             my_stack.display();*/
-
-            /*  QueueUsingLinkedList my_queue = new QueueUsingLinkedList();
-              my_queue.e*/
-
-            Solution solution = new Solution();
-            int[] marks = new int[5] { 0, 2, 2, 3, 4 };
-            int[] arr = solution.duplicates(marks, 5);
-            foreach (int x in arr)
-            {
-                Console.WriteLine(x);
-            }
-
-
-
-
+              my_stack.push(70);
+              my_stack.push(40);
+              my_stack.push(77);
+              my_stack.push(798);
+              my_stack.pop();
+              my_stack.display();
+ */
+            QueueUsingLinkedList my_queue = new QueueUsingLinkedList();
+            my_queue.Enqueue(50);
+            my_queue.Enqueue(40);
+            my_queue.Enqueue(56);
 
 
         }
--- StackAndQueue/StackAndQueue/QueueUsingLinkedList.cs
+++ StackAndQueue/StackAndQueue/QueueUsingLinkedList.cs
@@ -11,46 +11,47 @@ namespace StackAndQueue
         {
             public int data;
             public Node next;
-            public Node(int data)
-            {
-                this.data = data;
-                this.next = null;
-            }
         }
-        public class Queue
+
+        Node front;
+        Node rear;
+        public QueueUsingLinkedList()
         {
-            Node front, rear;
-
-            public Queue()
-            {
-                this.front = null;
-                this.rear = null;
-            }
-
-            public void Enqueue(int data)
-            {
-
-                Node temp = new Node(data);
-
-                if (rear == null)
-                {
-                    this.front = this.rear = temp;
-                }
-                this.rear.next = temp;
-                this.rear = temp;
-            }
-
-            public void dequeue()
-            {
-                if (this.front == null)
-                    Console.WriteLine("queue is empty");
-
-                Node temp = this.front;
-                this.front = this.front.next;
-
-                if (this.front == null)
-                    this.rear = null;
-            }
+            this.front = null;
+            this.rear = null;
         }
+
+
+
+
+        public void Enqueue(int data)
+        {
+
+            Node temp = new Node();
+
+            if (rear == null)
+            {
+                this.front = this.rear = temp;
+            }
+            this.rear.next = temp;
+            this.rear = temp;
+            Console.WriteLine("added " + data);
+
+        }
+
+        public void dequeue()
+        {
+            if (this.front == null)
+                Console.WriteLine("queue is empty");
+
+            Node temp = this.front;
+            this.front = this.front.next;
+
+            if (this.front == null)
+                this.rear = null;
+        }
+
+
+
     }
 }
